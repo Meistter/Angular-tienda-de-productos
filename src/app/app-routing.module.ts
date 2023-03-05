bce159ffdda454f43e6655790aabcea04bbcc445
Josue Perez <josueperez723@gmail.com>
2023-03-05 13:26:33 -0400

    precarga solo de modulos especificos custom.serv

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { CustomPreloadService } from './services/custom-preload.service';
 const routes: Routes = [
 
   //Esta es una forma de hacerlo, que cuando la ruta este vacia cargue home, o que el home sea la ruta vacia, o que redireccione a home al estar vacia
@@ -10,7 +11,10 @@ const routes: Routes = [
   // },
   {//aqui estamos importando el Modulo de administracion que creamos (cms)
     path: '',
-    loadChildren: ()=> import('../app/website/website.module').then(m => m.WebsiteModule)
+    loadChildren: ()=> import('../app/website/website.module').then(m => m.WebsiteModule),
+    data:{
+      preload: true
+    }
   },
   {//aqui estamos importando el Modulo de administracion que creamos (cms)
     path: 'admin',
@@ -24,7 +28,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: CustomPreloadService})],//aqui con el PreloadAllModules definimos que los modulos se carguen uno detras de otro a medida que este desocupado el explorador de forma que esten listos para cuando el usuario se dirija a ellos pero que no carguen todos a la vez en la entrada inicial a la web, esto es recomendable solo en paginas con pocos modulos
+  exports: [RouterModule]                                                          //Sin embargo en este caso en el servicio custom-preload definimos una estrategia de carga donde va a cargar solo los modulos con el preload = true, por eso nuestra estrategia de precarga aqui es un custom-service, el preload = true lo definimos en el app-routing.module principal para home y en el de website para category
 })
 export class AppRoutingModule { }
