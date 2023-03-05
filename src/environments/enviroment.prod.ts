e035a4e94dcf12e7fa00362bfa54c3b146028035
Josue Perez <josueperez723@gmail.com>
2023-03-04 20:30:57 -0400

    css

--- src/environments/enviroment.prod.ts
+++ src/environments/enviroment.prod.ts
@@ -1,4 +1,4 @@
-export const environment = {
-  production: true,
-  API : 'https://young-sands-07814.herokuapp.com'
-};
+// export const environment = {
+//   production: true,
+//   API : 'https://young-sands-07814.herokuapp.com'
+// };
--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
+  // production: false,
   API : 'https://young-sands-07814.herokuapp.com' //en desarrollo es vacia ya que estamos usando el proxy
 };
